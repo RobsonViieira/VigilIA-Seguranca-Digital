6f218f34833c802925f4a00fd35f67c70e9a967c
Robson Vieira <63127021+RobsonViieira@users.noreply.github.com>
2026-02-21 14:12:00 -0300

    Atualizar o auth.py

--- auth.py
+++ auth.py
@@ -1,13 +1,10 @@
 import hashlib
 from db import conectar
 
-
 def hash_senha(senha):
     return hashlib.sha256(senha.encode()).hexdigest()
 
-
 def criar_usuario(user, senha):
-
     con = conectar()
     cur = con.cursor()
 
@@ -16,19 +13,14 @@ def criar_usuario(user, senha):
             "INSERT INTO users(username,password) VALUES (?,?)",
             (user, hash_senha(senha))
         )
-
         con.commit()
         return True
-
     except:
         return False
-
     finally:
         con.close()
 
-
 def validar_login(user, senha):
-
     con = conectar()
     cur = con.cursor()
 
