881b6a6e782bedb1f10ff0e7ffacdd7798a829c8
Robson Vieira <63127021+RobsonViieira@users.noreply.github.com>
2026-02-21 14:29:54 -0300

    Atualizar o main.py

--- main.py
+++ main.py
@@ -4,14 +4,15 @@ import time
 from datetime import datetime
 
 ATTACK_TYPES = {
-    "Tentativa de login inválido": 1,
+    "Login inválido": 2,
     "IP desconhecido": 2,
-    "Acesso não autorizado": 3,
     "Transferência suspeita": 4,
-    "Ataque de força bruta": 5
+    "Força bruta": 5
 }
 
+
 def gerar_evento():
+
     ataque = random.choice(list(ATTACK_TYPES.keys()))
     base = ATTACK_TYPES[ataque]
 
@@ -25,25 +26,30 @@ def gerar_evento():
         nivel = "ALTO"
 
     hora = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
     return f"[{hora}] {ataque} | {nivel} ({score:.2f})"
 
+
 def salvar(evento):
+
     if not os.path.exists("logs"):
         os.mkdir("logs")
 
     with open("logs/seguranca.log", "a") as f:
         f.write(evento + "\n")
 
-def main():
-    print("Rovie IA iniciado...\n")
 
-    for _ in range(40):
+def main():
+
+    print("Rovie IA ativo...\n")
+
+    for _ in range(30):
+
         e = gerar_evento()
         print(e)
         salvar(e)
         time.sleep(1)
 
-    print("\nFinalizado.")
 
 if __name__ == "__main__":
     main()
