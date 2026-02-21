280f866f98b3d0cd90c5348844f8040fb0b4f2d6
Robson Vieira <63127021+RobsonViieira@users.noreply.github.com>
2026-02-21 14:32:06 -0300

    Atualizar o auth.py

--- auth.py
+++ auth.py
@@ -3,113 +3,102 @@ from datetime import datetime, timedelta
 from db import conectar
 
 
-def hash_senha(senha):
-    return hashlib.sha256(senha.encode()).hexdigest()
+def hash_senha(s):
+    return hashlib.sha256(s.encode()).hexdigest()
 
 
-def criar_usuario(user, senha):
+def criar_usuario(user, senha, admin=0):
 
     con = conectar()
     cur = con.cursor()
 
     try:
         cur.execute("""
-        INSERT INTO users(username,password,tentativas,bloqueado_ate)
-        VALUES (?,?,0,NULL)
-        """, (user, hash_senha(senha)))
+        INSERT INTO users(username,password,admin)
+        VALUES (?,?,?)
+        """,(user,hash_senha(senha),admin))
 
         con.commit()
         return True
-
     except:
         return False
-
     finally:
         con.close()
 
 
-def buscar_usuario(user):
+def buscar(user):
 
-    con = conectar()
-    cur = con.cursor()
+    con=conectar()
+    cur=con.cursor()
 
-    cur.execute("SELECT * FROM users WHERE username=?", (user,))
-    res = cur.fetchone()
+    cur.execute("SELECT * FROM users WHERE username=?",(user,))
+    r=cur.fetchone()
 
     con.close()
-    return res
+    return r
 
 
-def resetar_tentativas(user):
+def resetar(user):
 
-    con = conectar()
-    cur = con.cursor()
+    con=conectar()
+    cur=con.cursor()
 
     cur.execute("""
-    UPDATE users SET tentativas=0, bloqueado_ate=NULL WHERE username=?
-    """, (user,))
+    UPDATE users SET tentativas=0,bloqueado_ate=NULL WHERE username=?
+    """,(user,))
 
     con.commit()
     con.close()
 
 
-def registrar_erro(user):
+def erro(user):
 
-    con = conectar()
-    cur = con.cursor()
+    con=conectar()
+    cur=con.cursor()
 
     cur.execute("""
-    UPDATE users SET tentativas = tentativas + 1 WHERE username=?
-    """, (user,))
+    UPDATE users SET tentativas=tentativas+1 WHERE username=?
+    """,(user,))
 
     cur.execute("""
     SELECT tentativas FROM users WHERE username=?
-    """, (user,))
+    """,(user,))
 
-    t = cur.fetchone()[0]
+    t=cur.fetchone()[0]
 
-    # Bloqueia por 10 minutos após 3 erros
-    if t >= 3:
+    if t>=3:
 
-        bloqueio = (datetime.now() + timedelta(minutes=10)).strftime("%Y-%m-%d %H:%M:%S")
+        ate=(datetime.now()+timedelta(minutes=10)).strftime("%Y-%m-%d %H:%M:%S")
 
         cur.execute("""
         UPDATE users SET bloqueado_ate=? WHERE username=?
-        """, (bloqueio, user))
+        """,(ate,user))
 
     con.commit()
     con.close()
 
 
-def validar_login(user, senha):
+def validar(user,senha):
 
-    dados = buscar_usuario(user)
+    d=buscar(user)
 
-    if not dados:
-        return "NAO_EXISTE"
+    if not d:
+        return "NAO"
 
-    _, _, senha_hash, tentativas, bloqueado_ate = dados
+    _,_,hash_,tent,ate,admin=d
 
-    agora = datetime.now()
+    if ate:
+        fim=datetime.strptime(ate,"%Y-%m-%d %H:%M:%S")
 
-    # Verifica bloqueio
-    if bloqueado_ate:
+        if datetime.now()<fim:
+            return "BLOCK"
 
-        ate = datetime.strptime(bloqueado_ate, "%Y-%m-%d %H:%M:%S")
+        resetar(user)
 
-        if agora < ate:
-            return "BLOQUEADO"
+    if hash_==hash_senha(senha):
 
-        else:
-            resetar_tentativas(user)
-
-    # Verifica senha
-    if senha_hash == hash_senha(senha):
-
-        resetar_tentativas(user)
+        resetar(user)
         return "OK"
 
-    else:
-
-        registrar_erro(user)
-        return "ERRO"
+    erro(user)
+    return "ERRO"
