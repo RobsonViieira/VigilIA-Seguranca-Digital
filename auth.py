186452750b255cdae42329ceb9bef74843fd6795
Robson Vieira <63127021+RobsonViieira@users.noreply.github.com>
2026-02-21 14:05:02 -0300

    Criar auth.py

---
+++ auth.py
@@ -0,0 +1,43 @@
+import hashlib
+from db import conectar
+
+
+def hash_senha(senha):
+    return hashlib.sha256(senha.encode()).hexdigest()
+
+
+def criar_usuario(user, senha):
+
+    con = conectar()
+    cur = con.cursor()
+
+    try:
+        cur.execute(
+            "INSERT INTO users(username,password) VALUES (?,?)",
+            (user, hash_senha(senha))
+        )
+
+        con.commit()
+        return True
+
+    except:
+        return False
+
+    finally:
+        con.close()
+
+
+def validar_login(user, senha):
+
+    con = conectar()
+    cur = con.cursor()
+
+    cur.execute(
+        "SELECT * FROM users WHERE username=? AND password=?",
+        (user, hash_senha(senha))
+    )
+
+    res = cur.fetchone()
+    con.close()
+
+    return res is not None
