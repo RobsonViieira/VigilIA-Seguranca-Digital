c26cd90d9a18fdae7ec3eac71cbdac1b3c25907c
Robson Vieira <63127021+RobsonViieira@users.noreply.github.com>
2026-02-21 14:11:42 -0300

    Atualizar o db.py

--- db.py
+++ db.py
@@ -2,19 +2,16 @@ import sqlite3
 
 DB = "users.db"
 
-
 def conectar():
     return sqlite3.connect(DB)
 
-
 def criar_tabelas():
-
     con = conectar()
     cur = con.cursor()
 
     cur.execute("""
     CREATE TABLE IF NOT EXISTS users(
-        id INTEGER PRIMARY KEY,
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
         username TEXT UNIQUE,
         password TEXT
     )
